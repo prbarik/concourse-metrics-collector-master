98810e375b29f66891731947bd86a92aed8a7f51
prbarik <prbarik93@gmail.com>
2022-01-20 01:51:36 +0530

    My Initial Commit

---
+++ main.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"concourse-metrics-collector-master/cache"
+	"encoding/json"
+	"flag"
+	"io"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"code.cloudfoundry.org/credhub-cli/models"
+	"code.cloudfoundry.org/lager"
+	"github.com/concourse/concourse/atc"
+	"github.com/concourse/concourse/atc/event"
+	"github.com/concourse/concourse/fly/commands"
+	"github.com/concourse/concourse/fly/rc"
+	"github.com/concourse/concourse/go-concourse/concourse"
+	"github.com/pkg/errors"
+	"github.com/prbarik/concourse-metrics-collector-go/models"
+	"github.com/prbarik/concourse-metrics-collector-go/utils"
+)
+
+type ConcourseConfig struct {
+	Username string        `json:"username"`
+	Password string        `json:"password"`
+	URL      string        `json:"url"`
+	Team     string        `json:"team"`
+	Target   rc.TargetName `json:"target"`
+}
+
+type Config struct {
+	Concourse ConcourseConfig `json:"concourse"`
+}
+
+func ReadConfig(configPath string) (Config, error) {
+	raw, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return Config{}, errors.Wrap(err, "failed-to-read-config-file")
+	}
+
+	var config Config
+	err = json.Unmarshal(raw, &config)
+	if err != nil {
+		return Config{}, errors.Wrap(err, "failed-to-unmarshal-config")
+	}
+
+	return config, nil
+}
+
+func GetMetrics(client concourse.Client, build atc.Build) (models.BuildMetric, error) {
+	buildPublicPlan, found, err := client.BuildPlan(build.ID)
+	if err != nil {
+		return models.BuildMetric{}, errors.Wrap(err, "failed to get build plan")
+	}
+
+	if !found {
+		return models.BuildMetric{}, nil
+	}
+
+	var buildPlan atc.Plan
+	err = json.Unmarshal(*buildPublicPlan.Plan, &buildPlan)
+	if err != nil {
+		return models.BuildMetric{}, errors.Wrap(err, "failed to unmarshal build plan")
+	}
+
+	taskMetrics := utils.CollectIDs(buildPlan)
+
+	buildEvents, err := client.BuildEvents(strconv.Itoa(build.ID))
+	if err != nil {
+		return models.BuildMetric{}, errors.Wrap(err, "failed to get build events")
+	}
+
+	// https://github.com/concourse/concourse/blob/09aecaa35913a78a475f72abdb33783903fa3f3b/fly/eventstream/render.go
+	var events []atc.Event
+	for {
+		streamEvent, err := buildEvents.NextEvent()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return models.BuildMetric{}, errors.Wrap(err, "failed to get the next event")
+		}
+
+		events = append(events, streamEvent)
+	}
+
+	initializeTimes := map[string]int64{}
+	startTimes := map[string]int64{}
+	finishTimes := map[string]int64{}
+
+	for _, currentEvent := range events {
+		if strings.HasPrefix(string(currentEvent.EventType()), "initialize-") {
+			originID, timez, err := utils.GetOriginAndTime(currentEvent)
+			if err != nil {
+				return models.BuildMetric{}, errors.Wrap(err, "failed to get origin and time for an initialize- event")
+			}
+			initializeTimes[originID] = timez
+		}
+
+		if strings.HasPrefix(string(currentEvent.EventType()), "start-") {
+			originID, timez, err := utils.GetOriginAndTime(currentEvent)
+			if err != nil {
+				return models.BuildMetric{}, errors.Wrap(err, "failed to get origin and time for an start- event")
+			}
+			startTimes[originID] = timez
+		}
+
+		if strings.HasPrefix(string(currentEvent.EventType()), "finish-") {
+			originID, timez, err := utils.GetOriginAndTime(currentEvent)
+			if err != nil {
+				return models.BuildMetric{}, errors.Wrap(err, "failed to get origin and time for an finish- event")
+			}
+			finishTimes[originID] = timez
+		}
+	}
+
+	var allTaskMetrics []models.TaskMetric
+	for _, value := range taskMetrics {
+		value.BuildId = build.ID
+
+		value.InitializeTime = initializeTimes[value.ID]
+		value.StartTime = startTimes[value.ID]
+		value.FinishTime = finishTimes[value.ID]
+
+		allTaskMetrics = append(allTaskMetrics, value)
+	}
+
+	return models.BuildMetric{
+		ID:        build.ID,
+		Name:      build.Name,
+		Status:    build.Status,
+		StartTime: build.StartTime,
+		EndTime:   build.EndTime,
+
+		TeamName:     build.TeamName,
+		PipelineName: build.PipelineName,
+		JobName:      build.JobName,
+		Tasks:        allTaskMetrics,
+	}, nil
+}
+
+func GetToken(config Config) (rc.Target, error) {
+	target, err := rc.LoadTarget(config.Concourse.Target, false)
+	if err != nil {
+		return nil, errors.Wrap(err, "load target failed")
+	}
+
+	err = target.Validate()
+
+	if err != nil {
+		if strings.HasSuffix(err.Error(), "not authorized") {
+			commands.Fly.Target = config.Concourse.Target
+			login := &commands.LoginCommand{BrowserOnly: true}
+			err := login.Execute([]string{})
+			if err != nil {
+				return nil, errors.Wrap(err, "login command failed")
+			}
+		} else {
+			return nil, errors.Wrap(err, "validate failed")
+		}
+	}
+
+	return target, nil
+}
+
+func main() {
+	configPath := flag.String("config-path", "", "please provide the path to your config file")
+	cachePath := flag.String("cache-path", "", "please provide the path to your cache file")
+	flag.Parse()
+
+	logger := lager.NewLogger("blackbox")
+	logger.RegisterSink(lager.NewWriterSink(os.Stderr, lager.DEBUG))
+
+	//someTimeAgo := time.Now().AddDate(0, -3, 0)
+	//logger.Debug("some-time-ago", lager.Data{"someTimeAgo": someTimeAgo})
+
+	logger.Debug("reading-config", lager.Data{"configPath": *configPath})
+	config, err := ReadConfig(*configPath)
+	if err != nil {
+		logger.Fatal("reading-config-failed", err)
+	}
+
+	logger.Debug("reading-cache", lager.Data{"cachePath": *cachePath})
+	cacher, err := cache.NewCache(*cachePath)
+	if err != nil {
+		logger.Fatal("reading-cache-failed", err)
+	}
+
+	logger.Debug("get-token")
+	target, err := GetToken(config)
+	if err != nil {
+		logger.Fatal("get-token-failed", err)
+	}
+
+	// Should be fixed once this gets merged: https://github.com/concourse/concourse/pull/4291
+	event.RegisterEvent(event.InitializeGet{})
+	event.RegisterEvent(event.InitializePut{})
+
+	logger.Debug("filtering-builds")
+	builds := utils.FilterBuilds(target.Team(), time.Unix(0, 0))
+
+	for i, build := range builds {
+		if cacher.IsProcessed(build.ID) {
+			logger.Debug("build-already-processed", lager.Data{"build-id": build.ID})
+			continue
+		}
+
+		logger.Debug("getting-metrics", lager.Data{"build": build, "current-index": i, "max-index": len(builds)})
+		buildMetric, err := GetMetrics(target.Client(), build)
+		if err != nil {
+			logger.Fatal("get-metrics-failed", err)
+		}
+
+		utils.UglyPrint(buildMetric)
+
+		cacher.MarkProcessed(build.ID)
+	}
+}
---
+++ utiils/utils.go
@@ -0,0 +1,159 @@
+package utils
+
+import (
+	"concourse-metrics-collector-go/models"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/concourse/concourse/go-concourse/concourse"
+
+	"github.com/concourse/concourse/atc"
+	"github.com/pkg/errors"
+)
+
+type GenericEvent struct {
+	Origin struct {
+		ID string `json:"id"`
+	} `json:"origin"`
+	Time int64 `json:"time"`
+}
+
+func GetOriginAndTime(event atc.Event) (string, int64, error) {
+	eventBytes, err := json.Marshal(event)
+	if err != nil {
+		return "", 0, errors.Wrap(err, "event marshaling failed")
+	}
+
+	var genericEvent GenericEvent
+	err = json.Unmarshal(eventBytes, &genericEvent)
+	if err != nil {
+		return "", 0, errors.Wrap(err, "event unmarshaling failed")
+	}
+
+	return genericEvent.Origin.ID, genericEvent.Time, nil
+}
+
+func FilterBuilds(team concourse.Team, timeAgo time.Time) []atc.Build {
+	var builds []atc.Build
+
+	pipelines, _ := team.ListPipelines()
+	for _, pipeline := range pipelines {
+
+		jobs, _ := team.ListJobs(pipeline.Name)
+
+		for _, job := range jobs {
+			page := concourse.Page{Limit: 0}
+			jobBuilds, _, _, _ := team.JobBuilds(pipeline.Name, job.Name, page)
+
+			for _, jobBuild := range jobBuilds {
+				buildTime := time.Unix(jobBuild.EndTime, 0)
+
+				if jobBuild.IsRunning() || jobBuild.OneOff() {
+					continue
+				}
+
+				if buildTime.After(timeAgo) {
+					builds = append(builds, jobBuild)
+				}
+			}
+		}
+	}
+
+	return builds
+}
+
+func CollectIDs(plan atc.Plan) map[string]models.TaskMetric {
+	plans := []atc.Plan{
+		plan,
+	}
+
+	taskMetrics := map[string]models.TaskMetric{}
+
+	for len(plans) > 0 {
+		currentPlan := plans[0]
+		plans = plans[1:] //pop
+
+		currentPlanID := string(currentPlan.ID)
+
+		if currentPlan.Task != nil {
+			taskMetrics[currentPlanID] = models.TaskMetric{
+				ID:   currentPlanID,
+				Name: currentPlan.Task.Name,
+				Type: "task",
+			}
+		}
+
+		if currentPlan.Get != nil {
+			taskMetrics[currentPlanID] = models.TaskMetric{
+				ID:   currentPlanID,
+				Name: currentPlan.Get.Name,
+				Type: "get",
+			}
+		}
+
+		if currentPlan.Put != nil {
+			taskMetrics[currentPlanID] = models.TaskMetric{
+				ID:   currentPlanID,
+				Name: currentPlan.Put.Name,
+				Type: "put",
+			}
+		}
+
+		if currentPlan.Aggregate != nil {
+			plans = append(plans, *currentPlan.Aggregate...)
+		}
+
+		if currentPlan.InParallel != nil {
+			plans = append(plans, currentPlan.InParallel.Steps...)
+		}
+
+		if currentPlan.Do != nil {
+			plans = append(plans, *currentPlan.Do...)
+		}
+
+		if currentPlan.OnAbort != nil {
+			plans = append(plans, currentPlan.OnAbort.Step, currentPlan.OnAbort.Next)
+		}
+
+		if currentPlan.OnError != nil {
+			plans = append(plans, currentPlan.OnError.Step, currentPlan.OnError.Next)
+		}
+
+		if currentPlan.Ensure != nil {
+			plans = append(plans, currentPlan.Ensure.Step, currentPlan.Ensure.Next)
+		}
+
+		if currentPlan.OnSuccess != nil {
+			plans = append(plans, currentPlan.OnSuccess.Step, currentPlan.OnSuccess.Next)
+		}
+
+		if currentPlan.OnFailure != nil {
+			plans = append(plans, currentPlan.OnFailure.Step, currentPlan.OnFailure.Next)
+		}
+
+		if currentPlan.Try != nil {
+			plans = append(plans, currentPlan.Try.Step)
+		}
+
+		if currentPlan.Timeout != nil {
+			plans = append(plans, currentPlan.Timeout.Step)
+		}
+
+		if currentPlan.Retry != nil {
+			plans = append(plans, *currentPlan.Retry...)
+		}
+	}
+
+	return taskMetrics
+}
+
+func PrettyPrint(v interface{}) {
+	b, _ := json.MarshalIndent(v, "", "  ")
+	fmt.Println(string(b))
+}
+
+func UglyPrint(v interface{}) {
+	b, _ := json.Marshal(v)
+	fmt.Println(string(b))
+}
